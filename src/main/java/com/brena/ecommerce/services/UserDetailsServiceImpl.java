0ffc5abae894e5a7d46a861b97926350f8a027f1
Brena Andring <bzandring@gmail.com>
2020-04-08 16:46:00 -0700

    updated

--- src/main/java/com/brena/ecommerce/services/UserDetailsServiceImpl.java
+++ src/main/java/com/brena/ecommerce/services/UserDetailsServiceImpl.java
@@ -14,26 +14,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class UserDetailsServiceImplementation implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserRepo userRepo;
-    public UserDetailsServiceImplementation(UserRepo userRepo) {
+
+    public UserDetailsServiceImpl(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepo.findByEmail(email);
 
-        if(user == null) {
+        if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthorities(user));
     }
 
-    private List<GrantedAuthority> getAuthorities(User user){
+    private List<GrantedAuthority> getAuthorities(User user) {
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-        for(Role role : user.getRoles()) {
+        for (Role role : user.getRoles()) {
             GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getName());
             authorities.add(grantedAuthority);
         }
