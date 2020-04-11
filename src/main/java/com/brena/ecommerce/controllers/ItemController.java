e78c72f729561b385e2f243854a76fd38d4f40d2
Brena Andring <bzandring@gmail.com>
2020-04-11 10:24:14 -0700

    updated

--- src/main/java/com/brena/ecommerce/controllers/ItemController.java
+++ src/main/java/com/brena/ecommerce/controllers/ItemController.java
@@ -2,7 +2,6 @@ package com.brena.ecommerce.controllers;
 
 import com.brena.ecommerce.models.*;
 import com.brena.ecommerce.services.ItemServ;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
