a5fa474cad3d642b8014870c508546034d115521
Brena Andring <bzandring@gmail.com>
2020-05-01 21:24:19 -0700

    updated

--- src/main/java/com/brena/ecommerce/repositories/CartRepo.java
+++ src/main/java/com/brena/ecommerce/repositories/CartRepo.java
@@ -1,4 +1,8 @@
 package com.brena.ecommerce.repositories;
 
-public class CartRepo {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepo extends CrudRepository {
 }
