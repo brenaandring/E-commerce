fa634139fd0b8161949b28e84d89594002827e75
Brena Andring <bzandring@gmail.com>
2020-05-02 22:55:50 -0700

    updated

--- src/main/java/com/brena/ecommerce/controllers/CartController.java
+++ src/main/java/com/brena/ecommerce/controllers/CartController.java
@@ -1,4 +1,7 @@
 package com.brena.ecommerce.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CartController {
 }
--- src/main/java/com/brena/ecommerce/services/CartServ.java
+++ src/main/java/com/brena/ecommerce/services/CartServ.java
@@ -1,4 +1,7 @@
 package com.brena.ecommerce.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartServ {
 }
