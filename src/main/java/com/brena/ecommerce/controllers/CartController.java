6cf5e9d485090ecd35907a5453e5f331cfd6bc92
Brena Andring <bzandring@gmail.com>
2020-05-03 23:27:35 -0700

    added attributes and constructor

--- src/main/java/com/brena/ecommerce/controllers/CartController.java
+++ src/main/java/com/brena/ecommerce/controllers/CartController.java
@@ -1,7 +1,16 @@
 package com.brena.ecommerce.controllers;
 
+import com.brena.ecommerce.services.ItemServ;
+import com.brena.ecommerce.services.UserServ;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CartController {
+    private final UserServ userServ;
+    private final ItemServ itemServ;
+
+    public CartController(UserServ userServ, ItemServ itemServ) {
+        this.userServ = userServ;
+        this.itemServ = itemServ;
+    }
 }
