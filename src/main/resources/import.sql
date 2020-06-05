9d1106f493552c069e81ced30f2257fe84fe2b6e
Brena Andring <bzandring@gmail.com>
2020-06-04 17:40:00 -0700

    compressed images and added gzip/cache header

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,7 +13,7 @@ INSERT INTO items (item_id, description, price, quantity, title, user_id) VALUES
 INSERT INTO items (item_id, description, price, quantity, title, user_id) VALUES ('4', 'face mask', '10', '5', 'SUMMER FESTIVAL', '1');
 
 
-INSERT INTO photos (photo_id, file_name, item_id) VALUES ('1', '772f721c-24f1-4f0b-a332-2d04b68ed544.png', '4');
-INSERT INTO photos (photo_id, file_name, item_id) VALUES ('2', '14d94d51-6623-41de-909f-84d1cda25bea.png', '3');
-INSERT INTO photos (photo_id, file_name, item_id) VALUES ('3', 'dfaae724-5e4f-4252-af12-b452f5ef50f0.png', '2');
-INSERT INTO photos (photo_id, file_name, item_id) VALUES ('4', '6382465f-615e-4947-b107-f5f5afd4ea92.png', '1');
+INSERT INTO photos (photo_id, file_name, item_id) VALUES ('1', 'imgonline-com-ua-CompressToSize-QHKwrszMz9.jpg', '4');
+INSERT INTO photos (photo_id, file_name, item_id) VALUES ('2', 'imgonline-com-ua-CompressToSize-eNiL60H89AOZih.jpg', '3');
+INSERT INTO photos (photo_id, file_name, item_id) VALUES ('3', 'imgonline-com-ua-CompressToSize-ZssOcYNChQ.jpg', '2');
+INSERT INTO photos (photo_id, file_name, item_id) VALUES ('4', 'imgonline-com-ua-CompressToSize-6k2G26brzqBBwnp.jpg', '1');
