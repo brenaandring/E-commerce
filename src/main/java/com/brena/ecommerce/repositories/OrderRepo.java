2c03c92ecc9348e514e2f08bfd347063441cbeba
Brena Andring <bzandring@gmail.com>
2020-06-11 18:06:27 -0700

    changed repo to jparepo

--- src/main/java/com/brena/ecommerce/repositories/OrderRepo.java
+++ src/main/java/com/brena/ecommerce/repositories/OrderRepo.java
@@ -1,22 +1,17 @@
 package com.brena.ecommerce.repositories;
 
 import com.brena.ecommerce.models.Order;
-import com.brena.ecommerce.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface OrderRepo extends CrudRepository<Order, Long> {
+public interface OrderRepo extends JpaRepository<Order, Long> {
     Page<Order> findAll(Pageable pageable);
 
-    Order findByUser(User user);
-
     Optional<Order> findById(Long id);
-
-    List<Order> findByStatus(String status);
 }
