7a4d40840d7f6d8d2f430d9c21c7d95b30c9e56e
Brena Andring <bzandring@gmail.com>
2020-06-29 20:13:11 -0700

    updated aws config class

--- src/main/java/com/brena/ecommerce/config/AwsS3Config.java
+++ src/main/java/com/brena/ecommerce/config/AwsS3Config.java
@@ -10,22 +10,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AWSS3Config {
+public class AwsS3Config {
 
-    // Access key id will be read from the application.properties file during the application intialization.
     @Value("${aws.access_key_id}")
     private String accessKeyId;
-    // Secret access key will be read from the application.properties file during the application intialization.
+
     @Value("${aws.secret_access_key}")
     private String secretAccessKey;
-    // Region will be read from the application.properties file  during the application intialization.
+
     @Value("${aws.s3.region}")
     private String region;
 
     @Bean
     public AmazonS3 getAmazonS3Cient() {
         final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
-        // Get AmazonS3 client and return the s3Client object.
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion(Regions.fromName(region))
