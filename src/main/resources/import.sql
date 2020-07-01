5a4609f940da345836bdbb375d413cf144d77a96
Brena Andring <bzandring@gmail.com>
2020-07-01 15:32:22 -0700

    updated sql script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
 INSERT INTO roles (role_id, name) VALUES ('1', 'ROLE_ADMIN');
 INSERT INTO roles (role_id, name) VALUES ('2', 'ROLE_USER');
 
-INSERT INTO users (user_id, email, first_name, last_name, password) VALUES ('1', 'brena@gmail.com', 'Brena', 'Andring', '$2a$10$y9e6FPktlBFaUfJSisIs8ekXrp2rVOMFnz.JcBaiprMorAFdS1xWa');
+# INSERT INTO users (user_id, email, first_name, last_name, password) VALUES ('1', 'brena@gmail.com', 'Brena', 'Andring', '$2a$10$y9e6FPktlBFaUfJSisIs8ekXrp2rVOMFnz.JcBaiprMorAFdS1xWa');
 # INSERT INTO users (user_id, email, first_name, last_name, password) VALUES ('2', 'barnes@gmail.com', 'Barnes', 'Angel', '$2a$10$ietdK5wFLS8j6dLhfr731e/YfgURsAZa1oUVnbEOEl1cjDlHrSbLi');
 #
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+# INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
 # INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
 #
 # INSERT INTO items (item_id, description, price, quantity, title, user_id) VALUES ('1', 'face mask', '10', '5', 'AQUA', '1');
